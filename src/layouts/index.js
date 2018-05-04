26049ee39851d7e379fb488d6c460b0a557b0e84
Chris Lee <sirleech@gmail.com>
2018-05-05 09:02:00 +1000

    Update site helmet

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Chris Gatsby CMS" />
     <Navbar />
     <div>{children()}</div>
   </div>
